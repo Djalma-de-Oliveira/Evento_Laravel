c5f67e51d3e6fd10b19eace8608d0bc65dfa0d2b
“Djalma-de-Oliveiragit config --global user.name “DjalmadeOliveira <djalma07031971@gmail.com>
2022-08-04 00:33:02 -0300

    First Commit

---
+++ app/Models/User.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class User extends Model
+{
+    use HasFactory;
+
+    protected $table = 'users';
+
+    protected $fillable = [
+        'name',
+        'email',
+        'cpf',
+        'password',
+        'role'
+
+    ];
+
+    protected $hidden = [
+        'password'
+    ];
+
+    //mutators
+    public function setPasswordAttribute($value)
+    {
+        $this->attributes['password'] = bcrypt($value);
+    }
+}
